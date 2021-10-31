d8bfd920f0cbfe8649e95b468933a8506a6b50af
Abhishek Krishna <abhishekkrishna@Abhisheks-MacBook-Air.local>
2021-10-31 22:49:26 +0530

    Completed sign-in and sign-up and integrated firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Route , Switch } from 'react-router';
-import Header from './Components/header/header.component';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shopPage/shopPage.component';
-import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
-import './App.css';
-import { auth } from './firebase/firebase.utils'
+import { Switch, Route } from 'react-router-dom';
 
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shopPage/shopPage.component'
+import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
+import Header from './Components/header/header.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -20,10 +21,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
@@ -31,18 +45,17 @@ class App extends React.Component {
     this.unsubscribeFromAuth();
   }
 
-
-  render(){
+  render() {
     return (
-      <div className="App">
-        <Header currentUser = {this.state.currentUser}/>
+      <div>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/signIn" component={SignInAndSignUpPage} />
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </div>
-    )
+    );
   }
 }
 
